0ed0c63d43cc6e6b0a3600ebfd8e007c00b337f7
nhut.truong <tlmnhut2207@gmail.com>
2021-12-27 19:22:47 +0100

    update path

--- test_compare.py
+++ test_compare.py
@@ -6,7 +6,7 @@ from compare_rep import measure_change
 def test():
     # CCA
     print('\nCCA')
-    with open('./result_compare/node/lenet5/cifar10/feat_mats_instance', 'rb') as f:
+    with open('./results_compare/node/lenet5/cifar10/feat_mats_instance', 'rb') as f:
         feat_mats_instance = pickle.load(f)
 
     cca_u_p, cca_c_n = [], []
@@ -20,7 +20,7 @@ def test():
 
     # Spearman
     print('\nSpearman')
-    with open('./result_compare/node/lenet5/cifar10/feat_mats_concept', 'rb') as f:
+    with open('./results_compare/node/lenet5/cifar10/feat_mats_concept', 'rb') as f:
         feat_mats_concept = pickle.load(f)
 
     spearman_u_p, spearman_c_n = [], []
