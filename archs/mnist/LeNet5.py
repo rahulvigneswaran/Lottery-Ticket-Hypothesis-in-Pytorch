205bdbaef5e10ca75486507d647d7717c4a4e8b4
nhut.truong <tlmnhut2207@gmail.com>
2021-12-27 14:58:07 +0100

    to extract layers

--- archs/mnist/LeNet5.py
+++ archs/mnist/LeNet5.py
@@ -1,26 +1,22 @@
-import torch
+import torch.nn.functional as func
 import torch.nn as nn
 
 class LeNet5(nn.Module):
     def __init__(self, num_classes=10):
         super(LeNet5, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=(3, 3), stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(64*14*14, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, 256),
-            nn.ReLU(inplace=True),
-            nn.Linear(256, num_classes),
-        )
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=1, padding=1)
+        self.fc1 = nn.Linear(64*14*14, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.features(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = func.relu(self.conv1(x))
+        x = func.relu(self.conv2(x))
+        x = func.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
+
